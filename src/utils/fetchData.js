bde771f6e0c248107bf8d9108448b04f2eaac7a8
SeasonLeague <facebookups23@gmail.com>
2023-06-03 15:00:06 +0100

    Initial commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+﻿export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'e5c039e6e7msh013425c48454853p157efbjsnebdbb788322f',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
